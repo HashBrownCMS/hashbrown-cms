15031e7ad4db8711c83038e3925254837d6140bd
mrzapp <contact@jeppezapp.com>
2016-03-05 11:57:28 +0100

    mongo

--- src/server/helpers/ContentHelper.js
+++ src/server/helpers/ContentHelper.js
@@ -2,19 +2,66 @@
 
 // MongoDB client
 let mongodb = require('mongodb');
-let MongoClient = mongodb.MongoClient;
+let mongoClient = mongodb.MongoClient;
+let mongoDatabase;
 
 // Config
 let config = require('../../../config.json');
 
 class ContentHelper {
     /**
+     * Inits the MongoDB database
+     */
+    static initMongo() {
+        return new Promise(function(callback) {
+            if(!mongoDatabase) {
+                MongoClient.connect(env.connectionString, function(connectErr, db) {
+                    if(db) {
+                        mongoDatabase = db;
+                        callback(mongoDatabase);
+
+                    } else {
+                        throw 'Couldn\'t connect to MongoDB. Please check the /config.json file and ensure that the credentials provided are correct.';
+                    
+                    }
+                });
+                    
+            } else {
+                callback(mongoDatabase);
+            
+            }
+        });
+    }
+
+    /**
+     * Finds a single MongoDb document
+     *
+     * @param {Object} query
+     *
+     * @return {Promise} promise
+     */
+    static mongoFindOne(collectionName, query) {
+        return new Promise(function(callback) {
+                    mongoDatabase.collection(collectionName).findOne({id: id}, function(findErr, doc) {
+                        if(findErr) {
+                            throw findErr;
+                        }
+
+                        callback(doc);
+                    });
+            });
+        });
+    }
+    
+    /**
      * Gets a Page object by id
      *
      * @param {Number} id
+     *
+     * @return {Promise} promise
      */
     static getPageById(id) {
-        return {};
+        });
     }
     
     /**
