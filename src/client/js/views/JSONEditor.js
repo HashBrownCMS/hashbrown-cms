71fcf5a2e52f7477df502ef2038f363c71f4b96e
mrzapp <contact@jeppezapp.com>
2016-03-05 14:01:46 +0100

    json editor

--- src/client/js/views/JSONEditor.js
+++ src/client/js/views/JSONEditor.js
@@ -4,17 +4,31 @@ class JSONEditor extends View {
     constructor(params) {
         super(params);
 
-        this.$element = _.div({class: 'json-editor'});
+        this.$element = _.div({class: 'json-editor flex-vertical'});
         
         this.fetch();
     }
 
+    onClickReload() {
+
+    }
+
+    onClickSave() {
+        
+    }
+
     render() {
-        this.$element.html(
-            _.textarea(
+        this.$element.html([
+            _.textarea({class: 'flex-expand'},
                 JSON.stringify(this.model)
-            )
-        );
+            ),
+            _.div({class: 'btn-group flex-horizontal'}, [
+                _.button({class: 'btn btn-primary flex-expand'}, 'Reload'),
+                    .click(this.onClickReload)
+                _.button({class: 'btn btn-success flex-expand'}, 'Save')
+                    .click(this.onClickSave)
+            ])
+        ]);
     }
 }
 
