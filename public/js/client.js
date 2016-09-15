45aae8e65f82528baa74b8cca9908fb3361ad7e1
mrzapp <contact@jeppezapp.com>
2016-09-15 19:57:26 +0200

    fixed array sorting issue

--- public/js/client.js
+++ public/js/client.js
@@ -38153,16 +38153,21 @@ class ArrayEditor extends View {
         this.value.schemaBindings.splice(i, 1);
         this.value.items.splice(i, 1);
 
-        this.render();
+        this.$element.this.updateDOMIndices();
     }
 
     /**
      * Event: Click add item
      */
     onClickAddItem() {
-        this.value.items.push(null);
+        let index = this.value.items.length;
 
-        this.render();
+        this.value.items[index] = null;
+        this.value.schemaBindings[index] = null;
+
+        this.$element.children('.items').append(this.renderItem(index, null));
+
+        this.updateDOMIndices();
     }
 
     /**
@@ -38200,39 +38205,121 @@ class ArrayEditor extends View {
         this.$keyContent.find('.text-sorting').toggle(isSorting);
 
         if (isSorting) {
-            this.$element.find('.item').each((oldIndex, item) => {
+            this.$element.children('.items').children('.item').each((oldIndex, item) => {
                 $(item).exodragdrop({
                     lockX: true,
                     dropContainers: this.$element[0].querySelectorAll('.items'),
                     scrollContainer: document.querySelector('.content-editor .nav-tabs'),
                     onEndDrag: instance => {
-                        let newIndex = $(instance.element).index();
+                        this.updateDOMIndices();
+
+                        let newIndex = parseInt(instance.element.dataset.index);
 
                         // Change the index in the items array
-                        let value = this.value.items[oldIndex];
-                        let itemsClone = this.value.items.slice();
-                        itemsClone.splice(oldIndex, 1);
-                        itemsClone.splice(newIndex, 0, value);
-                        this.value.items = itemsClone;
+                        let item = this.value.items[oldIndex];
+                        this.value.items.splice(oldIndex, 1);
+                        this.value.items.splice(newIndex, 0, item);
 
                         // Change the index in the schema bindings array
                         let schema = this.value.schemaBindings[oldIndex];
-                        let bindingsClone = this.value.schemaBindings.slice();
-                        bindingsClone.splice(oldIndex, 1);
-                        bindingsClone.splice(newIndex, 0, schema);
-                        this.value.schemaBindings = bindingsClone;
-
-                        // Change the index in the element
-                        $(instance.element).attr('data-array-index', newIndex);
-
-                        oldIndex = newIndex;
+                        this.value.schemaBindings.splice(oldIndex, 1);
+                        this.value.schemaBindings.splice(newIndex, 0, schema);
                     }
                 });
             });
         } else {
-            this.$element.find('.item').each(function () {
+            this.updateDOMIndices();
+
+            this.$element.children('.items').children('.item').each(function () {
                 $(this).exodragdrop('destroy');
             });
+        }
+    }
+
+    /**
+     * Updates DOM indices
+     */
+    updateDOMIndices() {
+        this.$element.children('.items').children('.item').each((i, element) => {
+            element.dataset.index = i;
+        });
+    }
+
+    /**
+     * Renders an item
+     *
+     * @param {Number} index
+     * @param {Object} item
+     *
+     * @returns {HTMLElement} Item
+     */
+    renderItem(index, item) {
+        // Sanity check for item schema
+        if (!this.config.allowedSchemas) {
+            this.config.allowedSchemas = [];
+        }
+
+        let itemSchemaId = this.value.schemaBindings[index];
+
+        // Schema could not be found, assign first allowed schema
+        if (this.config.allowedSchemas.length > 0 && (!itemSchemaId || this.config.allowedSchemas.indexOf(itemSchemaId) < 0)) {
+            itemSchemaId = this.config.allowedSchemas[0];
+            this.value.schemaBindings[index] = itemSchemaId;
+        }
+
+        // Make sure we have the item schema and the editor we need for each array item
+        let itemSchema = resources.schemas[itemSchemaId];
+
+        if (itemSchema) {
+            let fieldEditor = resources.editors[itemSchema.editorId];
+
+            // Perform sanity check and reassign the item into the array
+            item = ContentHelper.fieldSanityCheck(item, itemSchema);
+            this.value.items[index] = item;
+
+            // Returns the correct index, even if it's updated
+            let getIndex = () => {
+                return $element.attr('data-index');
+            };
+
+            // Init the schema selector
+            let $schemaSelector = _.div({ class: 'item-schema-selector kvp' }, _.div({ class: 'key' }, 'Schema'), _.div({ class: 'value' }, _.select({ class: 'form-control' }, _.each(this.config.allowedSchemas, (allowedSchemaIndex, allowedSchemaId) => {
+                let allowedSchema = resources.schemas[allowedSchemaId];
+
+                return _.option({ value: allowedSchemaId }, allowedSchema.name);
+            })).on('change', () => {
+                itemSchemaId = $schemaSelector.find('select').val();
+
+                this.value.schemaBindings[getIndex()] = itemSchemaId;
+
+                this.render();
+            }).val(itemSchemaId)));
+
+            // Set schema label (used when sorting items)
+            let $schemaLabel = _.span({ class: 'schema-label' }, itemSchema.name);
+
+            // Init the field editor
+            let fieldEditorInstance = new fieldEditor({
+                value: itemSchema.multilingual ? item[window.language] : item,
+                disabled: itemSchema.disabled || false,
+                config: itemSchema.config || {},
+                schema: itemSchema
+            });
+
+            // Hook up the change event
+            fieldEditorInstance.on('change', newValue => {
+                this.onChange(newValue, getIndex(), itemSchema);
+            });
+
+            // Return the DOM element
+            let $element = _.div({ class: 'item raised' }, _.button({ class: 'btn btn-embedded btn-remove' }, _.span({ class: 'fa fa-remove' })).click(() => {
+                this.onClickRemoveItem(getIndex());
+                $element.remove();
+            }), $schemaLabel, this.config.allowedSchemas.length > 1 ? $schemaSelector : null, fieldEditorInstance.$element);
+
+            return $element;
+        } else {
+            debug.warning('Schema by id "' + itemSchemaId + '" not found', this);
         }
     }
 
@@ -38268,80 +38355,16 @@ class ArrayEditor extends View {
         _.append(this.$element.empty(), _.div({ class: 'items' },
         // Loop through each array item
         _.each(this.value.items, (i, item) => {
-            // Sanity check for item schema
-            if (!this.config.allowedSchemas) {
-                this.config.allowedSchemas = [];
-            }
-
-            let itemSchemaId = this.value.schemaBindings[i];
-
-            if (this.config.allowedSchemas.length > 0 && (!itemSchemaId || this.config.allowedSchemas.indexOf(itemSchemaId) < 0)) {
-                itemSchemaId = this.config.allowedSchemas[0];
-                this.value.schemaBindings[i] = itemSchemaId;
-            }
-
-            // Make sure we have the item schema and the editor we need for each array item
-            let itemSchema = resources.schemas[itemSchemaId];
-
-            if (itemSchema) {
-                let fieldEditor = resources.editors[itemSchema.editorId];
-
-                // Sanity check
-                item = ContentHelper.fieldSanityCheck(item, itemSchema);
-                this.value.items[i] = item;
-
-                // Init the schema selector
-                let $schemaSelector = _.div({ class: 'item-schema-selector kvp' }, _.div({ class: 'key' }, 'Schema'), _.div({ class: 'value' }, _.select({ class: 'form-control' }, _.each(this.config.allowedSchemas, (i, allowedSchemaId) => {
-                    let allowedSchema = resources.schemas[allowedSchemaId];
-
-                    return _.option({ value: allowedSchemaId }, allowedSchema.name);
-                })).on('change', () => {
-                    let index = $element.attr('data-array-index');
-
-                    itemSchemaId = $schemaSelector.find('select').val();
-
-                    this.value.schemaBindings[index] = itemSchemaId;
-
-                    this.trigger('change', this.value);
-
-                    this.render();
-                }).val(itemSchemaId)));
-
-                // Set schema label (used when sorting items)
-                let $schemaLabel = _.span({ class: 'schema-label' }, itemSchema.name);
-
-                // Init the field editor
-                let fieldEditorInstance = new fieldEditor({
-                    value: itemSchema.multilingual ? item[window.language] : item,
-                    disabled: itemSchema.disabled || false,
-                    config: itemSchema.config || {},
-                    schema: itemSchema
-                });
-
-                // Hook up the change event
-                fieldEditorInstance.on('change', newValue => {
-                    let index = $element.attr('data-array-index');
-
-                    this.onChange(newValue, index, itemSchema);
-                });
-
-                // Return the DOM element
-                let $element = _.div({ class: 'item raised', 'data-array-index': i }, _.button({ class: 'btn btn-embedded btn-remove' }, _.span({ class: 'fa fa-remove' })).click(() => {
-                    let index = $element.attr('data-array-index');
-
-                    this.onClickRemoveItem(index);
-                }), $schemaLabel, this.config.allowedSchemas.length > 1 ? $schemaSelector : null, fieldEditorInstance.$element);
-
-                return $element;
-            } else {
-                debug.warning('Schema by id "' + itemSchemaId + '" not found', this);
-            }
+            return this.renderItem(i, item);
         })),
 
         // Render the add item button
         _.button({ class: 'btn btn-primary btn-raised btn-add-item btn-round' }, '+').click(() => {
             this.onClickAddItem();
         }));
+
+        // Update indices
+        this.updateDOMIndices();
     }
 }
 
