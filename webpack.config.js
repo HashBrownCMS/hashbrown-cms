33f2db224bebb34d785072853f0813961413e05c
mrzapp <git@yggdrasil>
2020-01-09 11:14:33 +0000

    Tidied up the webpack cfg

--- webpack.config.js
+++ webpack.config.js
@@ -6,46 +6,67 @@ const Webpack = require('webpack');
 const Sass = require('sass/sass.dart.js');
 const FileSystem = require('fs');
 
-// Observe changes for specific files
-let entry = {
-    dashboard: './src/Client/dashboard.js',
-    demo: './src/Client/demo.js',
-    environment: './src/Client/environment.js',
+// Parameters
+const IS_WATCHING = Array.isArray(process.argv) && process.argv.indexOf('--watch') > -1;
+const TMP_PATH = Path.join(__dirname, 'tmp');
+const PLUGINS_PATH = Path.join(__dirname, 'plugins');
 
-    common: './src/Common',
-    service: './src/Client/Service',
-    entity: './src/Client/Entity',
-    utilities: './src/Client/utilities'
-}
+/**
+ * Gets all .js entries
+ */
+function getJsEntries() {
+    // Include main codebase files
+    let entry = {
+        dashboard: './src/Client/dashboard.js',
+        demo: './src/Client/demo.js',
+        environment: './src/Client/environment.js',
 
-// Include plugins
-let pluginsPath = Path.join(__dirname, 'plugins');
-let pluginFiles = [];
-
-if(FileSystem.existsSync(pluginsPath)) {
-    for(let plugin of FileSystem.readdirSync(pluginsPath)) {
-        let indexPath = Path.join(pluginsPath, plugin, 'src', 'Client', 'index.js');
-
-        if(!FileSystem.existsSync(indexPath)) { continue; }
-
-        pluginFiles.push(indexPath);
+        common: './src/Common',
+        service: './src/Client/Service',
+        entity: './src/Client/Entity',
+        utilities: './src/Client/utilities'
     }
-}
 
-if(pluginFiles.length > 0) {
-    entry.plugins = pluginFiles;
-}
+    // Include plugins
+    let pluginScripts = [];
 
-// Are we watching for changes?
-let isWatching = false;
+    if(FileSystem.existsSync(PLUGINS_PATH)) {
+        for(let plugin of FileSystem.readdirSync(PLUGINS_PATH)) {
+            let scriptPath = Path.join(PLUGINS_PATH, plugin, 'src', 'Client', 'index.js');
 
-// Process input arguments
-if(Array.isArray(process.argv)) {
-    for(let arg of process.argv) {
-        if(arg === '--watch') {
-            isWatching = true;
+            if(!FileSystem.existsSync(scriptPath)) { continue; }
+
+            pluginScripts.push(scriptPath);
         }
     }
+
+    if(pluginScripts.length > 0) {
+        entry.plugins = pluginScripts;
+    }
+
+    return entry;
+}
+
+/**
+ * Runs the SASS compilation
+ */
+function compileCss() {
+    // Build SASS commands
+    let sassArgs = [];
+
+    if(IS_WATCHING) { 
+        sassArgs.push('--watch');
+        sassArgs.push('--poll');
+    }
+
+    sassArgs.push('--source-map');
+    sassArgs.push('--embed-sources'),
+
+    // Include main codebase styling
+    sassArgs.push('./style/index.scss:./public/css/style.css');
+
+    // Start SASS compilation
+    Sass.run_(sassArgs);
 }
 
 // Define settings
@@ -54,7 +75,7 @@ module.exports = {
     devtool: 'source-map',
 
     // Input .js
-    entry: entry,
+    entry: getJsEntries(),
 
     // Output .js
     output: {
@@ -69,16 +90,5 @@ module.exports = {
     }
 };
 
-// Compile SASS
-// NOTE: We're compiling SASS separately, since depending on the WebPack process is too slow
-let sassArgs = [];
-
-if(isWatching) { 
-    sassArgs.push('--watch');
-}
-
-sassArgs.push('--source-map');
-sassArgs.push('--embed-sources'),
-sassArgs.push('./style/index.scss:./public/css/style.css');
-
-Sass.run_(sassArgs);
+// Compile CSS
+compileCss();
