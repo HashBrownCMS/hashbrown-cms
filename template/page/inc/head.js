58c5350f1d274c395b06235a075ab53e88993ba9
mrzapp <em@il.jeppez.app>
2020-12-10 15:43:38 +0100

    Updated favicon

--- template/page/inc/head.js
+++ template/page/inc/head.js
@@ -10,7 +10,7 @@ module.exports = (_, model) => `
 <meta name="description" content="A free and open-source headless CMS">
 <meta name="robots" content="noindex, nofollow, noarchive, nosnippet">
 
-<link href="${model.rootUrl}/favicon.png" rel="icon" type="image/png">
+<link href="${model.rootUrl}/svg/favicon.svg" rel="icon" type="image/svg">
 <link href="${model.rootUrl}/lib/normalize/normalize.css" rel="stylesheet">
 <link href="${model.rootUrl}/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
