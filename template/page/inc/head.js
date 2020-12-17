db28b32c4eb9e40475686630b1830fc4af6eb022
mrzapp <em@il.jeppez.app>
2020-12-17 11:29:40 +0100

    Font fix for custom root URL

--- template/page/inc/head.js
+++ template/page/inc/head.js
@@ -10,6 +10,26 @@ module.exports = (_, model) => `
 <meta name="description" content="A free and open-source headless CMS">
 <meta name="robots" content="noindex, nofollow, noarchive, nosnippet">
 
+<style>
+    @font-face {
+        font-family: 'NunitoSans';
+        font-weight: 400;
+        src: url('${model.context.config.system.rootUrl}/fonts/NunitoSans/NunitoSans-Regular.ttf');
+    }
+
+    @font-face {
+        font-family: 'NunitoSans';
+        font-weight: 700;
+        src: url('${model.context.config.system.rootUrl}/fonts/NunitoSans/NunitoSans-Bold.ttf');
+    }
+
+    @font-face {
+        font-family: 'NunitoSans';
+        font-weight: 100;
+        src: url('${model.context.config.system.rootUrl}/fonts/NunitoSans/NunitoSans-ExtraLight.ttf');
+    }
+</style>
+
 <link href="${model.context.config.system.rootUrl}/svg/favicon.svg" rel="icon" type="image/svg">
 <link href="${model.context.config.system.rootUrl}/lib/normalize/normalize.css" rel="stylesheet">
 <link href="${model.context.config.system.rootUrl}/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
